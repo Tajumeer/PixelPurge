666c7d2021c3449e1c974a9f219fd3c742b5798c
tajumeer <becker.sven@hotmail.com>
2024-01-24 17:45:09 +0100

    added Pull Request to firestoreREST

--- Assets/_Networking/FirestoreREST.cs
+++ Assets/_Networking/FirestoreREST.cs
@@ -1,34 +1,117 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SocialPlatforms.Impl;
+using System.Net;
+using System.Text;
 
-public class FirestoreRETS : MonoBehaviour
+public class FirestoreExample : MonoBehaviour
 {
-    [SerializeField] private string baseURL;
-
-    private void Start()
+    [System.Serializable]
+    public class FirestoreResponse
     {
-        StartCoroutine(GetFirestoreData("Leaderboard"));
+        public List<Document> Documents;
     }
 
-    IEnumerator GetFirestoreData(string _collectionName)
+    [System.Serializable]
+    public class Document
     {
-        string url = $"{baseURL}/{_collectionName}";
+        public string Name;
+        public Fields Fields;
+    }
 
-        using (UnityWebRequest www = UnityWebRequest.Get(url))
+    [System.Serializable]
+    public class Fields
+    {
+        public FirestoreValue Score;
+        public FirestoreValue User;
+    }
+
+    [System.Serializable]
+    public class FirestoreValue
+    {
+        public string IntegerValue;
+        public string StringValue;
+    }
+
+    [SerializeField] private string m_collection;
+    [SerializeField] private string m_baseURL;
+
+    void Start()
+    {
+        StartCoroutine(GetDataFromFirestore(m_collection));
+        StartCoroutine(SaveUserData(m_collection, "Tajumeer", 20000));
+    }
+
+    public IEnumerator GetDataFromFirestore(string _collection)
+    {
+        string url = m_baseURL + "/" + _collection;
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
         {
-            yield return www.SendWebRequest();
+            yield return webRequest.SendWebRequest();
 
-            if(www.isNetworkError || www.isHttpError)
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError)
             {
-                Debug.Log($"Error: {www.error}");
+                Debug.LogError("Error: " + webRequest.error);
             }
             else
             {
-                Debug.Log($"Response: {www.downloadHandler.text}");
+                string jsonResponse = webRequest.downloadHandler.text;
+                Debug.Log("Response: " + jsonResponse);
+
+                // Deserialize the JSON response
+                FirestoreResponse response = JsonConvert.DeserializeObject<FirestoreResponse>(jsonResponse);
+
+                if (response.Documents != null)
+                {
+                    foreach (var document in response.Documents)
+                    {
+                        string user = document.Fields.User?.StringValue ?? "";
+                        string score = document.Fields.Score?.IntegerValue ?? "";
+                        //TODO: Display Leaderboard
+                        Debug.Log("User: " + user + ", Score: " + score);
+                    }
+                }
+                else
+                {
+                    Debug.Log("Database is empty");
+                }
             }
         }
     }
 
+    public IEnumerator SaveUserData(string _collection, string _userName, int _score)
+    {
+        string url = m_baseURL + "/" + _collection;
+
+        string jsonPayload = "{" +
+          "\"fields\": {" +
+              "\"User\": {\"stringValue\": \"" + _userName + "\"}," +
+              "\"Score\": {\"integerValue\": \"" + _score.ToString() + "\"}" +
+          "}" +
+        "}";
+
+        using (UnityWebRequest webRequest = new UnityWebRequest(url, "POST"))
+        {
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonPayload);
+            webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            webRequest.downloadHandler = new DownloadHandlerBuffer();
+            webRequest.SetRequestHeader("Content-Type", "application/json");
+
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.Log("Error: " + webRequest.error);
+            }
+            else
+            {
+                string jsonResponse = webRequest.downloadHandler.text;
+                Debug.Log("Response: " + jsonResponse);
+            }
+        }
+    }
 }
