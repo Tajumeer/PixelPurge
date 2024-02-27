bf166e47bd58026d11283684abe400771de4c93a
Ayunay <maya.jabs@yahoo.de>
2024-02-27 14:44:57 +0100

    Exit Game

--- Assets/_Scripts/UI/Menus/PauseManager.cs
+++ Assets/_Scripts/UI/Menus/PauseManager.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
-    [SerializeField] private GameObject m_villageButton;
+    [SerializeField] private TextMeshProUGUI m_exitButtonText;
 
     private void OnEnable()
     {
-        if (SceneManager.GetSceneByBuildIndex((int)Scenes.Village).isLoaded) m_villageButton.SetActive(false);
-        else m_villageButton.SetActive(true);
+        if (SceneManager.GetSceneByBuildIndex((int)Scenes.Village).isLoaded) m_exitButtonText.text = "Exit";
+        else m_exitButtonText.text = "Village";
     }
+
     public void LoadOptions()
     {
         MenuManager.Instance.LoadSceneAsync(Scenes.Options, CursorTypes.UI, LoadSceneMode.Additive);
@@ -20,7 +23,7 @@ public class PauseManager : MonoBehaviour
     public void UnloadPause()
     {
         // if we are in the dungeon
-        if (FindObjectOfType<DungeonHUDManager>() != null)
+        if (SceneManager.GetSceneByBuildIndex((int)Scenes.Dungeon).isLoaded)
             FindObjectOfType<DungeonHUDManager>().UnloadPause();
 
         // or in the village
@@ -31,8 +34,22 @@ public class PauseManager : MonoBehaviour
         }
     }
 
-    public void UnloadDungeon()
+    public void Exit()
     {
-        MenuManager.Instance.UnloadDungeon();
+        // if we are in the dungeon, go back to village
+        if (SceneManager.GetSceneByBuildIndex((int)Scenes.Dungeon).isLoaded)
+            MenuManager.Instance.UnloadDungeon();
+
+        // else quit game
+        else
+        {
+#if UNITY_STANDALONE
+            Application.Quit();
+#endif
+
+#if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+#endif
+        }
     }
 }
